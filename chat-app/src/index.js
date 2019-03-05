f17f9fbbf2d035cab2456ef7912aab9003d8ec03
Ryan Hotton <ryanhotton@gmail.com>
2019-03-05 17:55:05 -0500

    S17 L158

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -22,6 +22,10 @@ io.on('connection', (socket) => {
         io.emit('message', message)
     })
 
+    socket.on('sendLocation', (coords) => {
+        socket.broadcast.emit('message', `https://www.google.ca/maps?q=${coords.latitude},${coords.longitude}`)
+    })
+
     socket.on('disconnect', () => {
         io.emit('message', 'A user has left!')
     })
